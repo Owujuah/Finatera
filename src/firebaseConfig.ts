e67d9f64d7b3741d75c5e60bff45732d946925f0
Owujuah <sunnyotaba@Owujuah.local>
2025-04-03 12:13:36 +0100

    update

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwA-3aIDDfDdeti_C4SsJKd9UbNGNLIAI",
+  authDomain: "unity-grande-bank-59c36.firebaseapp.com",
+  projectId: "unity-grande-bank-59c36",
+  storageBucket: "unity-grande-bank-59c36.firebasestorage.app",
+  messagingSenderId: "889405433905",
+  appId: "1:889405433905:web:cb1e8e8d2636980dfacd10"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
